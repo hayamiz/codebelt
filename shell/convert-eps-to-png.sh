787767eebff3516a113e9a1be5287d2a42dc576f
Yuto Hayamizu <y.hayamizu@gmail.com>
2013-08-17 19:43:28 +0900

    updated latex skeleton

--- shell/convert-eps-to-png.sh
+++ shell/convert-eps-to-png.sh
@@ -8,3 +8,6 @@ convert -background white -density 200 -rotate 90 FILE1 FILE2
 
 # rorate 90 for gnuplot graphs, and fill background with HEX color spec (#FFFF00 = yellow here)
 convert -background '#ffff00' -density 200 -rotate 90 FILE1 FILE2
+
+# rorate 90 for gnuplot graphs, and disable alpha channel
+convert -alpha Off -density 200 -rotate 90 FILE1 FILE2
